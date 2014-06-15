f8229b4ad72b969573a61dd6d584b4f4666f5277
befall <beholdtheleviathan@gmail.com>
2014-06-15 09:30:31 -0700

    changed stuff

--- web.js
+++ web.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-app.engine('html', require('ejs').__express);
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
 	res.render('about.html');
