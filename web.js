36ec32ff5654927f50bf371120adc05ac3114929
befall <beholdtheleviathan@gmail.com>
2014-06-15 14:36:19 -0700

    mustacheExpress added and fixes

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var mustache = require('mustache');
+var mustacheExpress = require('mustache-express');
 var app = express();
 
-app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/static'));
 app.use('/components', express.static(__dirname + '/bower_components'));
-app.engine('html', require('ejs').renderFile);
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
 	res.render('index.html');
