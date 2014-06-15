6647555e8183bd35bb93c3c7d282945e5780f8db
befall <beholdtheleviathan@gmail.com>
2014-06-14 19:26:44 -0700

    fixed html

--- web.js
+++ web.js
@@ -8,4 +8,7 @@ app.get('/', function(req, res) {
 	res.render('about.html');
 });
 
-app.listen(1337);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
