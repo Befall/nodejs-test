6dd1f0ae556d4f4cb7e510b2452f902d41365b00
befall <beholdtheleviathan@gmail.com>
2014-06-15 14:22:02 -0700

    bower and such

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/static'));
+app.use('/components', express.static(__dirname + '/bower_components'));
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
-	res.render('about.html');
+	res.render('index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
