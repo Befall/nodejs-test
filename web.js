acd2fa571b83e4b4a4f7b9f601f65a64fa527de9
befall <beholdtheleviathan@gmail.com>
2014-06-14 18:24:04 -0700

    lolwut

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
 // web.js
  var express = require("express");
- var logfmt = require("logfmt");
  var app = express();
 
- app.use(logfmt.requestLogger());
 
- app.get('/', function(req, res) {
+ app.get('/hello.txt', function(req, res) {
    res.send('Hello World!');
    });
 
